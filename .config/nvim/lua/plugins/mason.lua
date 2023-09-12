57f81311218c7c54af6fee67da43981d06594881
Jonas Eana <jonas@eana.ro>
2023-09-12 19:51:44 +0200

    Configure mason, neo-tree and treesitter

---
+++ .config/nvim/lua/plugins/mason.lua
@@ -0,0 +1,29 @@
+return {
+  "williamboman/mason.nvim",
+  -- keys = {
+  --   { "<leader>cm", false },
+  -- },
+  opts = {
+    ensure_installed = {
+      "bash-language-server",
+      -- "deno",
+      -- "diagnostic-languageserver",
+      -- "dockerfile-language-server",
+      "hadolint",
+      "helm-ls",
+      "json-lsp",
+      "jsonnet-language-server",
+      "lua-language-server",
+      "marksman",
+      "prettierd",
+      "shellcheck",
+      "shfmt",
+      "sqlls",
+      "stylua",
+      "terraform-ls",
+      "tflint",
+      -- "typescript-language-server",
+      "yaml-language-server",
+    },
+  },
+}
