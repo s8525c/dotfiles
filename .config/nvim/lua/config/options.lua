22e9ac279cba2ce01cf0cd78181f72c527f96488
Jonas Eana <jonas.eana@seb.se>
2023-10-02 13:58:36 +0200

    Configure shfmt formatter

--- .config/nvim/lua/config/options.lua
+++ .config/nvim/lua/config/options.lua
@@ -2,34 +2,35 @@
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
 
-local global = vim.g
-local options = vim.opt
-local command = vim.cmd
-
+-- local global = vim.g
 -- global.mapleader = " "
 -- global.maplocalleader = " "
+
+local options = vim.opt
+
+options.colorcolumn = "80,120"
+
+-- options.listchars = "eol:↴,tab:>·,trail:~,extends:>,precedes:<,space:·"
+-- set listchars=tab:→\ ,trail:·,nbsp:·
+options.listchars = "tab:>→,trail:·,nbsp:·"
+options.list = true
 
 -- options.guifont = "FiraCode NF:h14"
 options.clipboard = "unnamedplus"
 
 options.conceallevel = 0
 
-options.updatetime = 100
-options.scrolloff = 9999
+-- options.updatetime = 100
+-- options.scrolloff = 9999
 
 options.tabstop = 2
-options.colorcolumn = "80,120"
 options.shiftwidth = 2
+options.softtabstop = 2
 options.expandtab = true
 options.signcolumn = "yes"
 
 options.number = true
 options.relativenumber = false
-
--- options.listchars = "eol:↴,tab:>·,trail:~,extends:>,precedes:<,space:·"
--- set listchars=tab:→\ ,trail:·,nbsp:·
-options.listchars = "eol:↴,tab:>→,trail:·,nbsp:·"
-options.list = true
 
 options.background = "dark"
 options.termguicolors = true
@@ -38,23 +39,3 @@ options.completeopt = "menuone,noselect"
 
 options.swapfile = false
 options.autoread = true
-
-command("match errorMsg /\\s\\+$/")
-command("set wrap linebreak")
-
-local autocmd = vim.api.nvim_create_autocmd
-local augroup = vim.api.nvim_create_augroup
-
-local line_toggle = augroup("line_toggle", { clear = true })
-autocmd("InsertLeave", {
-  callback = function()
-    vim.wo.relativenumber = true
-  end,
-  group = line_toggle,
-})
-autocmd("InsertEnter", {
-  callback = function()
-    vim.wo.relativenumber = false
-  end,
-  group = line_toggle,
-})
--- .config/nvim/lua/plugins/mason.lua
+++ .config/nvim/lua/plugins/mason.lua
@@ -6,12 +6,11 @@ return {
   opts = {
     ensure_installed = {
       "bash-language-server",
-      -- "deno",
-      -- "diagnostic-languageserver",
-      -- "dockerfile-language-server",
+      "dockerfile-language-server",
       "hadolint",
       "helm-ls",
       "json-lsp",
+      "jsonlint",
       "jsonnet-language-server",
       "lua-language-server",
       "marksman",
@@ -22,8 +21,46 @@ return {
       "stylua",
       "terraform-ls",
       "tflint",
-      -- "typescript-language-server",
       "yaml-language-server",
+      "yamlfmt",
     },
+  },
+  {
+    "nvimtools/none-ls.nvim",
+    -- see https://github.com/jose-elias-alvarez/null-ls.nvim/blob/main/doc/BUILTINS.md#code-actions
+    opts = function()
+      local nls = require("null-ls")
+      local formatting = nls.builtins.formatting
+      local diagnostics = nls.builtins.diagnostics
+
+      return {
+        sources = {
+
+          -- formatting.prettierd.with({extra_args = {"--no-semi", "--single-qoute", "--jsx-single-qoute"}}),
+          formatting.prettierd,
+
+          -- lua
+          formatting.stylua,
+          diagnostics.luacheck.with({
+            condition = function(utils)
+              return utils.root_has_file(".luacheckrc")
+            end,
+          }),
+
+          -- yaml
+          formatting.yamlfmt,
+
+          -- shell
+          formatting.shfmt.with({ extra_args = { "-i", "4", "-ci" } }),
+          diagnostics.shellcheck.with({ diagnostics_format = "[shellcheck] #{m} [#{c}]" }),
+
+          -- json
+          diagnostics.jsonlint,
+
+          -- markdown
+          diagnostics.markdownlint,
+        },
+      }
+    end,
   },
 }
