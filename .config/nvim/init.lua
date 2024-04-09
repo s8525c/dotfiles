8dec10a34fe80758633099843144a8d02d458448
Jonas Eana <jonas.eana@seb.se>
2024-04-09 14:26:55 +0200

    Remove more frustrations

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -1,7 +1,9 @@
 -- bootstrap lazy.nvim, LazyVim and your plugins
 require("config.lazy")
 
--- Remap dd to _dd to avoid cutting when deleting
-vim.api.nvim_set_keymap("n", "dd", '"_dd', { noremap = true, silent = true })
+-- Remove frustrations
+vim.api.nvim_set_keymap("n", "C", '"_C', { noremap = true, silent = true })
+vim.api.nvim_set_keymap("n", "D", '"_D', { noremap = true, silent = true })
 vim.api.nvim_set_keymap("n", "d", '"_d', { noremap = true, silent = true })
+vim.api.nvim_set_keymap("n", "dd", '"_dd', { noremap = true, silent = true })
 vim.api.nvim_set_keymap("n", "x", '"_x', { noremap = true, silent = true })
